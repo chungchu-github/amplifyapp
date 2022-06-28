e646f02873be9ab22ee47ac82f2394648885751d
chungchu-github <chungchu@cisco.com>
2022-06-28 19:09:41 +0800

    hello lucas

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Lucas !!!
         </a>
       </header>
     </div>
